d460f70034aa160a860e0bf662a97e1b6bc17527
jiaaren <jiaren.kjr@gmail.com>
2021-08-19 19:39:20 +0800

    Working version with pthread)_join, for norminette clean up

--- submit/philo/srcs/philo_main.c
+++ submit/philo/srcs/philo_main.c
@@ -6,13 +6,14 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 17:46:14 by jkhong            #+#    #+#             */
-/*   Updated: 2021/08/19 17:32:45 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/08/19 19:38:09 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libphilo.h"
 
 static bool				g_simulate = false;
+static bool				g_eaten_enough = false;
 static pthread_mutex_t	*g_forks;
 static t_globals		g_args;
 
@@ -24,31 +25,6 @@ unsigned long givetime()
 	return((_timeval.tv_sec * (unsigned long)1000 + (_timeval.tv_usec / 1000)) % 1000000);
 }
 
-void	*test_thread(void *arg)
-{
-	int	i;
-
-	i = 0;
-	while (g_simulate)
-	{
-		if (i == 5)
-			break ;
-		printf("Starting thread %i\n", i++);
-		sleep(1);
-	}
-	g_simulate = false;
-	return (NULL);
-}
-
-/*
-	Philo cycle
-	- Think
-	- Pick up forks (mutexes)
-	- eat food (i.e. set time eaten)
-		- how to have philosopher check time when holding fork??
-	- sleep
-*/
-
 void	takefork(t_philo *philo, int fork)
 {
 	pthread_mutex_lock(&(g_forks[fork]));
@@ -59,9 +35,7 @@ void	takefork(t_philo *philo, int fork)
 void	putfork(t_philo *philo)
 {
 	pthread_mutex_unlock(&(g_forks[philo->fork_one]));
-	pthread_mutex_unlock(&(g_forks[philo->fork_two]));	
-	// // if (g_simulate)
-	// 	printf("%lu %i puts down forks\n", givetime(), philo->philo_num);
+	pthread_mutex_unlock(&(g_forks[philo->fork_two]));
 }
 
 void	end_cycle(void)
@@ -74,30 +48,43 @@ void	end_cycle(void)
 
 void	*death_thread(void *arg)
 {
-	const t_philo *philo = (t_philo *)(arg);
+	t_philo			*philo;
+	unsigned long	wait_time;
+	unsigned long	curr_time;
 
-	usleep(g_args.time_to_die * 1000);
-	if (givetime() > (philo->last_eat_time + g_args.time_to_die) || g_args.philo_amount == 1)
+	philo = (t_philo *)(arg);
+	wait_time = g_args.time_to_die;
+	while (!(philo->last_eat_time))
+		;
+	while (g_simulate)
 	{
-		if (g_simulate)
-			printf("%lu %i died\n", givetime(), philo->philo_num);
-		end_cycle();
-		putfork((t_philo *)philo);
+		usleep(wait_time * 1000);
+		curr_time = givetime();
+		wait_time = (philo->last_eat_time + g_args.time_to_die) - curr_time;
+		if (curr_time > (philo->last_eat_time + g_args.time_to_die) || g_args.philo_amount == 1)
+		{
+			if (g_simulate)
+				printf("%lu %i died\n", givetime(), philo->philo_num);
+			end_cycle();
+			putfork(philo);
+			break ;
+		}
+		
 	}
 }
 
 void	eat(t_philo *philo)
 {
-	pthread_t tmp_thread;
-
 	philo->last_eat_time = givetime();
-	pthread_create(&tmp_thread, NULL, death_thread, (void *)philo);
-	pthread_detach(tmp_thread);
 	if (g_simulate)
 		printf("%lu %i is eating\n", philo->last_eat_time, philo->philo_num);
 	(philo->times_eaten)++;
 	if (philo->times_eaten == g_args.times_philo_eat)
+	{
 		end_cycle();
+		g_eaten_enough = true;
+		putfork(philo);
+	}
 	usleep(g_args.time_to_eat * 1000);
 }
 
@@ -112,20 +99,16 @@ void	think(t_philo *philo)
 {
 	if (g_simulate)
 		printf("%lu %i is thinking\n", givetime(), philo->philo_num);
-	// to avoid double 'died'
 }
 
 void	*philo_cycle(void *arg)
 {
 	t_philo		*philo;
-	pthread_t	tmp_thread;
 
 	philo = (t_philo *)(arg);
-	while (!g_simulate)
+	while (!g_simulate && !g_eaten_enough)
 		;
 	philo->last_eat_time = givetime();
-	pthread_create(&tmp_thread, NULL, death_thread, (void *)philo);
-	pthread_detach(tmp_thread);
 	while (g_simulate)
 	{
 		think(philo);
@@ -135,10 +118,6 @@ void	*philo_cycle(void *arg)
 		putfork(philo);
 		philo_sleep(philo);
 	}
-	// how to avoid eating to stuck when 1 fork?
-	putfork(philo);
-	g_args.threads_ended++;
-	// ????
 	return (NULL);
 }
 
@@ -153,6 +132,7 @@ void	initialise_philo(int p_num, t_philo **philo)
 	{
 		tmp[i].philo_num = i + 1;
 		tmp[i].times_eaten = 0;
+		tmp[i].last_eat_time = 0;
 		if (i != p_num - 1)
 		{
 			tmp[i].fork_one = i;
@@ -180,22 +160,31 @@ void	initialise_phil_forks(int p_num, pthread_t **thread, pthread_mutex_t **fork
 	int				i;
 	pthread_t		*tmp_thread;
 	pthread_mutex_t	*tmp_forks;
+	pthread_t		tmp_thread2[5];
 
 	i = 0;
 	tmp_forks = malloc(sizeof(pthread_mutex_t) * p_num);
 	while (i < p_num)
 		pthread_mutex_init(&(tmp_forks[i++]), NULL);
 	*forks = tmp_forks;
-	i = -1;
+	i = 0;
 	tmp_thread = malloc(sizeof(pthread_t) * p_num);
-	while (++i < p_num)
-		pthread_create(&(tmp_thread[i]), NULL, philo_cycle, (void *)&(philo[i]));
+	while (i < p_num)
+	{
+		pthread_create(&(tmp_thread[i]), NULL, philo_cycle, (void *)(&(philo[i])));
+		pthread_create(&(philo[i].check_death), NULL, death_thread, (void *)(&(philo)[i]));
+		i++;
+	}
 	// these 2 had to be above
 	*thread = tmp_thread;
 	g_simulate = true;
 	i = 0;
 	while (i < p_num)
-		pthread_join(tmp_thread[i++], NULL);
+	{
+		pthread_join(tmp_thread[i], NULL);
+		pthread_join(philo[i].check_death, NULL);
+		i++;
+	}
 }
 
 void	free_threads(int p_num, pthread_t *thread, pthread_mutex_t *forks, t_philo *philo)
@@ -212,11 +201,11 @@ void	free_threads(int p_num, pthread_t *thread, pthread_mutex_t *forks, t_philo 
 
 void	initialise_globals(void)
 {
-	g_args.philo_amount = 5;
-	g_args.time_to_die = 200;
-	g_args.time_to_eat = 75;
-	g_args.time_to_sleep = 75;
-	g_args.times_philo_eat = 2;
+	g_args.philo_amount = 4;
+	g_args.time_to_die = 80;
+	g_args.time_to_eat = 50;
+	g_args.time_to_sleep = 50;
+	g_args.times_philo_eat = __UINT32_MAX__;
 }
 
 int	main(void)
@@ -228,9 +217,6 @@ int	main(void)
 	initialise_globals();
 	initialise_philo(g_args.philo_amount, &philo);
 	initialise_phil_forks(g_args.philo_amount, &thread, &g_forks, philo);
-	while (g_args.threads_ended != g_args.philo_amount);
-		;
-	usleep(500000);
 	free_threads(g_args.philo_amount, thread, g_forks, philo);
 	return (0);
 }
