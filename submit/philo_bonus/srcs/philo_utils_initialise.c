ac4cf1633c2f776a2cfda3936d29cdf22cab87f0
jiaaren <jiaren.kjr@gmail.com>
2021-08-27 00:38:07 +0800

    remove pthread_detach

--- submit/philo_bonus/srcs/philo_utils_initialise.c
+++ submit/philo_bonus/srcs/philo_utils_initialise.c
@@ -6,26 +6,26 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 18:51:44 by jkhong            #+#    #+#             */
-/*   Updated: 2021/08/21 21:27:58 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/08/27 00:37:13 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libphilo_bonus.h"
 
-void	wait_children(int p_num, int *child_pid)
+void wait_children(int p_num, int *child_pid)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while (i < p_num)
-		waitpid(child_pid[i++], NULL, 0);
+    i = 0;
+    while (i < p_num)
+        waitpid(child_pid[i++], NULL, 0);
 }
 
-void	initialise_philo(t_philo *philo)
+void initialise_philo(t_philo *philo)
 {
-	philo->philo_num = 0;
-	philo->last_eat_time = 0;
-	philo->times_eaten = 0;
+    philo->philo_num = 0;
+    philo->last_eat_time = 0;
+    philo->times_eaten = 0;
 }
 
 /*
@@ -33,30 +33,28 @@ void	initialise_philo(t_philo *philo)
 	free when pid == 0, and return NULL
 	if parent, return int array
 */
-int	*initialise_process(int p_num, t_philo *philo,
-		void *(death_cycle)(void *), void *(hear_one_death)(void *))
+int *initialise_process(int p_num, t_philo *philo,
+                        void *(death_cycle)(void *), void *(hear_one_death)(void *))
 {
-	int	*child_pid;
-	int	pid;
-	int	i;
+    int *child_pid;
+    int pid;
+    int i;
 
-	child_pid = malloc(sizeof(int) * p_num);
-	i = 0;
-	while (i < p_num)
-	{
-		philo->philo_num = 1 + i;
-		pid = fork();
-		if (pid == 0)
-			break ;
-		child_pid[i] = pid;
-		i++;
-	}
-	if (pid != 0)
-		return (child_pid);
-	free(child_pid);
-	pthread_create(&philo->th_death, NULL, death_cycle, (void *)0);
-	pthread_detach(philo->th_death);
-	pthread_create(&philo->th_hear_parent, NULL, hear_one_death, NULL);
-	pthread_detach(philo->th_hear_parent);
-	return (NULL);
+    child_pid = malloc(sizeof(int) * p_num);
+    i = 0;
+    while (i < p_num)
+    {
+        philo->philo_num = 1 + i;
+        pid = fork();
+        if (pid == 0)
+            break;
+        child_pid[i] = pid;
+        i++;
+    }
+    if (pid != 0)
+        return (child_pid);
+    free(child_pid);
+    pthread_create(&philo->th_death, NULL, death_cycle, (void *)0);
+    pthread_create(&philo->th_hear_parent, NULL, hear_one_death, NULL);
+    return (NULL);
 }
